7e9aedb7d5d4ae4c28a088c34c5a720b89cff6f7
Kristie Huntress <kristieh@su-d3.surgut.21-school.ru>
2025-08-13 22:46:00 +0500

    first commit

---
+++ Frontend/TicTacToe.Client/Program.cs
@@ -0,0 +1,33 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TicTacToe.Client;
+using TicTacToe.Client.Handlers;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var apiUrl = builder.Configuration.GetConnectionString("ApiConnection")
+   ?? "http://localhost:6543/";
+
+Console.WriteLine($"!!!!APIURL = {apiUrl}");
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<AuthHeaderHandler>();
+builder.Services.AddScoped<UserDataUpdateService>();
+builder.Services.AddScoped<RefreshDataService>();
+//builder.Services.AddScoped<GameHubService>();
+
+builder.Services.AddScoped<GameHubService>(provider => new GameHubService(apiUrl));
+
+builder.Services.AddHttpClient("HttpClient", client =>
+{
+    client.BaseAddress = new Uri(apiUrl);
+}).AddHttpMessageHandler<AuthHeaderHandler>();
+
+builder.Services.AddScoped(sp =>
+    sp.GetRequiredService<IHttpClientFactory>().CreateClient("HttpClient"));
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+await builder.Build().RunAsync();
